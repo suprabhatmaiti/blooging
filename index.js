b703aa109c8472185a6010d6f9a160f2147f4605
suprabhat maiti <maitisuprabhat@gmail.com>
2021-02-26 16:09:25 +0530

    Initial Setupp

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/check", (req, res) => {
+  res.send({ hello: "world" });
+});
+
+app.listen(4000, () => console.log("Backend running at 4000"));
