0746735e388076c3cc186a9e5b16c0bec2c43272
Subhankar Maiti <subhankarmaiti40@gmail.com>
2021-03-03 00:10:38 +0530

    swagger added

--- router/check.js
+++ router/check.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const route = express.Router();
 
+/**
+ * @openapi
+ * /check/hello:
+ *   get:
+ *     description: check API
+ *     responses:
+ *       200:
+ *         description: Returns dummy json
+ */
 route.get("/hello", (req, res) => {
   res.send({ hello: "world" });
 });
--- router/index.js
+++ router/index.js
@@ -1,4 +1,28 @@
-const check = require('./check');
-module.exports = function(app){
-    app.use('/check',check)
-}
+const check = require("./check");
+const swaggerUI = require("swagger-ui-express"); // to bind swagger with express and show the ui provided by swagger-js-doc
+const swaggerJSDoc = require("swagger-jsdoc"); // for API documentation
+module.exports = function (app) {
+  const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Swagger blogging API",
+        version: "1.0.0",
+      },
+      externalDocs: {
+        description: "Find out more about Swagger",
+        url: "http://swagger.io",
+      },
+      servers: [
+        {
+          url: "http://localhost:4000/",
+        },
+      ],
+    },
+    // path to api docs
+    apis: ["./router/check.js"],
+  };
+  const swaggerSpec = swaggerJSDoc(options);
+  app.use("/check", check);
+  app.use("/api-documentation", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+};
